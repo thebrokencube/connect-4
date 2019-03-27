53f5aafa416bb17a165477cd0c9c6004a6af2489
Sapan Upadhyay <sapan.upadhyay@gmail.com>
2019-03-27 01:05:00 -0700

    add column & row checking logic, and reset when someone wins

--- app/components/game-board.js
+++ app/components/game-board.js
@@ -23,21 +23,34 @@ export default Component.extend({
   numRows: 6,
   numColumns: 7,
   currentPlayer: null,
+  winner: null,
 
   init() {
     this._super();
+    this.reset();
+  },
+
+  reset() {
     this.set('board', generateBoard({ numColumns: this.numColumns, numRows: this.numRows }));
     this.set('currentPlayer', 0);
+    this.set('winner', null);
+  },
+
+  didRender() {
+    this.checkScore();
+    if (this.winner !== null) {
+      alert(`${this.winner} Wins!`);
+      return this.reset();
+    }
   },
 
   actions: {
     onClick(col, initialRow) {
       const row = this.calculateOpenCell(col);
+      if (row === null) { return; }
 
-      if (row !== null) {
-        const cell = this.board[col][row];
-        cell.set('color', this.playerColor());
-      }
+      const cell = this.board[col][row];
+      cell.set('color', this.playerColor());
 
       this.togglePlayer();
     }
@@ -61,5 +74,75 @@ export default Component.extend({
 
   togglePlayer() {
     this.set('currentPlayer', (this.currentPlayer + 1) % 2);
+  },
+
+  checkScore() {
+    let winner;
+
+    // check rows
+    this.checkRows();
+    if (this.winner !== null) { return; }
+
+    // check columns
+    this.checkColumns();
+    if (this.winner !== null) { return; }
+
+    // check left diagonals
+
+    // check right diagonals
+
+  },
+
+  checkRows() {
+    let consecutiveCount;
+    let lastColor;
+    let currentColor;
+
+    for (let row = 0; row < this.numRows; row++) {
+      consecutiveCount = 0;
+      lastColor = null;
+      currentColor = null;
+
+      for (let col = 0; col < this.numColumns; col++) {
+        currentColor = this.board[col][row].color;
+
+        if (lastColor === null || (currentColor !== WHITE && lastColor === currentColor)) {
+          consecutiveCount++;
+        } else {
+          consecutiveCount = 1;
+        }
+
+        if (consecutiveCount === 4) { return this.set('winner', this.currentPlayer); }
+
+        lastColor = currentColor;
+      }
+    }
+  },
+
+  checkColumns() {
+    let consecutiveCount;
+    let lastColor;
+    let currentColor;
+
+    for (let col = 0; col < this.numColumns; col++) {
+      consecutiveCount = 0;
+      lastColor = null;
+      currentColor = null;
+
+      for (let row = 0; row < this.numRows; row++) {
+        currentColor = this.board[col][row].color;
+
+        if (lastColor === null || (currentColor !== WHITE && lastColor === currentColor)) {
+          consecutiveCount++;
+        } else {
+          consecutiveCount = 1;
+        }
+
+        if (consecutiveCount === 4) { return this.set('winner', this.currentPlayer); }
+
+        lastColor = currentColor;
+      }
+    }
+    return null;
   },
 });
